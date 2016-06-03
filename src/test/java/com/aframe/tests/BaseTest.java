4c59ef924398ffe8eeab96d2027f2ccd5bcf9126
Ivan Voloshyn <a@a.com>
2016-06-03 19:54:53 +0300

    changed baseclass for using remote WebDriver

--- src/test/java/com/aframe/tests/BaseTest.java
+++ src/test/java/com/aframe/tests/BaseTest.java
@@ -2,12 +2,17 @@ package com.aframe.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.*;
 
+import javax.print.DocFlavor;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 
@@ -18,11 +23,26 @@ import java.util.concurrent.TimeUnit;
 public abstract class BaseTest {
 
     protected static WebDriver driver;
-    
+
+    DesiredCapabilities capability = DesiredCapabilities.firefox();
+
     //// TODO: 5/19/2016 read property and initialize appropriate driver (chrome, firefox or IE)
     protected WebDriver getWebdriver(){
+        /*
         if (driver == null) {
-            driver = new FirefoxDriver();
+            driver =  new FirefoxDriver();
+            driver.manage().deleteAllCookies();
+            //// TODO: 5/19/2016 - Add implicit wait
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+        }
+        */
+        if (driver == null) {
+            try {
+                driver =  new RemoteWebDriver(new URL("http://172.23.73.110:4444/wd/hub"), capability);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
             driver.manage().deleteAllCookies();
             //// TODO: 5/19/2016 - Add implicit wait
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
